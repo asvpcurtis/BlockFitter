9e241e27e0e37b35edf7e2b1251b7ababf31f72d
plzb0ss <curtisw0234@gmail.com>
2018-04-13 21:57:41 -0400

    Added current state of project Includes work on generating neighbour states

---
+++ BlockFitter/State.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockFitter
+{
+    class State
+    {
+        public BlockShape container;
+        public IEnumerable<BlockShape> pieces;
+        public State(BlockShape container, IEnumerable<BlockShape> pieces)
+        {
+            this.container = container;
+            this.pieces = pieces;
+        }
+
+        public State Clone()
+        {
+            return new State(container, pieces.Select(bs => bs.Copy()).ToList());
+        }
+
+        public IEnumerable<State> GetNeighbours()
+        {
+            List<State> neighbours = new List<State>();
+            for (int i = 0; i < pieces.Count(); i++)
+            {
+
+            }
+        }
+    }
+}
